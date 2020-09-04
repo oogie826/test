970ebab8d675286e500bffbc75b3a9b7a7e39b1c
oogie826 <oogie826@gmail.com>
2020-09-04 22:53:59 +0900

    Update styles

--- src/components/Navs.tsx
+++ src/components/Navs.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import SVG from '../components/SVG.tsx'
 
 import '../styles/Navs.scss'
 
@@ -8,7 +9,9 @@ export default function Navs() {
         <nav className='main-nav'>
             <h1 className='nav-title'><a href='/'>Kinder Guards</a></h1>
             <div className='nav-links'>
-                <Link className='nav-link-to' to='/search'>검색</Link>
+                <Link className='nav-link-to' to='/search'>
+                    <SVG className='svg-icon' name='search_icon' width={20} height={20} color='#000000'/>
+                </Link>
                 <Link className='nav-link-to' to='/login'>로그인</Link>
             </div>
         </nav>
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
+import SVG from '../components/SVG.tsx'
+
+import '../styles/Search.scss'
 
 export default function Search() {
+
+    const ref = useRef<HTMLInputElement>(null);
+    const [isFocus, setIsFocus] = useState(false);
+
+    const getFocus = (ev) => {
+        ref.current?.focus()
+    }
+
     return (
-        <div>
-            <input type='text'/>
-        </div>
+        <section className='search-section'>
+            <div className={`search-box ${isFocus ? 'focused' : 'unfocused'}`} onClick={getFocus}>
+                <SVG className='svg-icon' name='search_icon' width={30} height={30} />
+                <input ref={ref} className='search-input' type='text' 
+                    onFocus={() => setIsFocus(true)} 
+                    onBlur={() => setIsFocus(false)} 
+                    placeholder='검색어를 입력하세요'/>
+            </div>
+        </section>
     )
 }
